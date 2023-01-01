4423cb2a3d7dc76e05d0ee1e3b8d8542c8022cf2
GrandMaestier101 <95565677+GrandMaestier101@users.noreply.github.com>
2023-01-01 14:53:50 +0530

    Nonce reuse attack

--- solve.rb
+++ solve.rb
@@ -15,15 +15,11 @@ def sign(str)
     signature = ECDSA.sign($group, $private_key, digest, temp_key)
 end
 
-def verify?(str,signature)
-    digest = Digest::SHA256.digest(str)
-    ECDSA.valid_signature?($public_key, digest, signature)
-end
-
 user = "admin"
 sig = sign(user)
 cookies= Base64.strict_encode64(user+"--"+ECDSA::Format::SignatureDerString.encode(sig)) 
 f = File.open('output.txt', 'w')
-f.puts "sig #{sig}"
+f.puts "sig_s #{sig.s}"
+f.puts "sig_r #{sig.r}"
 f.puts "cookie #{cookies}"
 
