7b426cdc2a87b9bc7afd000b0312874d3719c2ed
GrandMaestier101 <95565677+GrandMaestier101@users.noreply.github.com>
2023-01-01 15:27:44 +0530

    Nonce reuse attack

--- solve.rb
+++ solve.rb
@@ -8,7 +8,8 @@ $group = ECDSA::Group::Secp256k1
 file = File.read('./output.json')
 data_hash = JSON.parse(file)
 $private_key =  data_hash['privateKey'] #92228332279050618974477679618746543127903843373270329631069559941710066834491
-puts "p #{$private_key}"
+public_key = $group.generator.multiply_by_scalar($private_key)
+
 def sign(str)
     digest = Digest::SHA256.digest(str) 
     temp_key = str.size 
@@ -22,4 +23,4 @@ f = File.open('output.txt', 'w')
 f.puts "sig_s #{sig.s}"
 f.puts "sig_r #{sig.r}"
 f.puts "cookie #{cookies}"
-
+f.puts "publicKey #{public_key.x}"
